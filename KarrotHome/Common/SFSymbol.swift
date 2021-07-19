d3f57bbdb7ae4dd45ea59d64fa25e65f0f824e4c
Jae-eun <je6752@gmail.com>
2021-07-20 05:18:01 +0900

    Add SFSymbol enum

---
+++ KarrotHome/Common/SFSymbol.swift
@@ -0,0 +1,20 @@
+//
+//  SFSymbol.swift
+//  KarrotHome
+//
+//  Created by 이재은 on 2021/07/20.
+//
+
+import Foundation
+
+enum SFSymbol: String {
+
+    case bell = "bell"
+    case bubbleLeftAndBubbleRight = "bubble.left.and.bubble.right"
+    case chevronDown = "chevron.down"
+    case chevronUp = "chevron.up"
+    case heart = "heart"
+    case listDash = "list.dash"
+    case magnifyingglass = "magnifyingglass"
+    case plus = "plus"
+}
